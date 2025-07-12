9c1d27d1a2890d4a6e090401b71fb80547a26c61
FTNeves80 <ftneves80@gmail.com>
2025-07-12 19:02:27 -0300

    while and try

--- tests/python_tutorial.py
+++ tests/python_tutorial.py
@@ -13,19 +13,31 @@ def days_to_units(num_of_days):
 
 # Function to check if the input is valid and perform the calculation    
 def check_input(input_value):
-    if user_input.isdigit():
+    #if user_input.isdigit():
+    try:
         user_input_number = int(input_value)
         if user_input_number > 0:
             calculated_value = days_to_units(user_input_number)
             print(calculated_value)
         elif user_input_number == 0:
             print("You entered 0 days, which is not a valid input for this calculation.")
-    else:
+        else:
+            print("You entered a negative number, which is not valid for this calculation.")
+    #else:
+    except ValueError:
         print("You entered an invalid value. Please enter a positive integer.")
 
+
+
 #User input
-user_input = input("Enter the number of days:\n")
-check_input(user_input)
+user_input = ""
+a = 0
+while user_input != "exit":
+    user_input = input("Enter the number of days:\n")
+    check_input(user_input)
+    a += 1
+    print(f"You have entered a value {a} times.")
+
 
 
 
